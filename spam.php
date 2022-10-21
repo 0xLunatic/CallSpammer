0a6da534ea6455de38984abb37d77913cec4940f
DreamerDotID <49750854+DreamerDotID@users.noreply.github.com>
2022-10-22 00:03:37 +0700

    Create spam.php

---
+++ spam.php
@@ -0,0 +1,44 @@
+<?php
+echo "
+$$$$$$\  $$\                            $$\     $$\           $$\                     $$\             $$\     
+$$  __$$\ $$ |                           $$ |    \__|          \__|                    \__|            $$ |    
+$$ /  \__|$$ |  $$\  $$$$$$\   $$$$$$\ $$$$$$\   $$\ $$\   $$\ $$\  $$$$$$\  $$$$$$$\  $$\  $$$$$$$\ $$$$$$\   
+\$$$$$$\  $$ | $$  |$$  __$$\ $$  __$$\\_$$  _|  $$ |\$$\ $$  |$$ |$$  __$$\ $$  __$$\ $$ |$$  _____|\_$$  _|  
+ \____$$\ $$$$$$  / $$$$$$$$ |$$ /  $$ | $$ |    $$ | \$$$$  / $$ |$$ /  $$ |$$ |  $$ |$$ |\$$$$$$\    $$ |    
+$$\   $$ |$$  _$$<  $$   ____|$$ |  $$ | $$ |$$\ $$ | $$  $$<  $$ |$$ |  $$ |$$ |  $$ |$$ | \____$$\   $$ |$$\ 
+\$$$$$$  |$$ | \$$\ \$$$$$$$\ $$$$$$$  | \$$$$  |$$ |$$  /\$$\ $$ |\$$$$$$  |$$ |  $$ |$$ |$$$$$$$  |  \$$$$  |
+ \______/ \__|  \__| \_______|$$  ____/   \____/ \__|\__/  \__|\__| \______/ \__|  \__|\__|\_______/    \____/ 
+                              $$ |                                                                             
+                              $$ |                                                                             
+                              \__|                                                                             
+\n\n";
+
+function spamCall($api, $nomer, $jumlah)
+{
+    $url = "https://spamertelpon.herokuapp.com/?key=" . $api . "&nohp=" . $nomer;
+    $loop = 0;
+    while ($loop < $jumlah) {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        $response = curl_exec($curl);
+        curl_close($curl);
+        $res = json_decode($response, true);
+        if ($res['status'] !== "success") {
+            echo "Minimal beli Api Key! \n";
+        } else {
+            echo "Spam ke " . $loop+1 . " berhasil dikirim! \n";
+        }
+        sleep(30);
+        $loop++;
+    }
+}
+
+echo "Masukan Nomor : ";
+$nomor = trim(fgets(STDIN));
+echo "Masukan Jumlah : ";
+$jumlah = trim(fgets(STDIN));
+echo "Masukan Key : ";
+$key = trim(fgets(STDIN));
+
+spamCall($key, $nomor, $jumlah);

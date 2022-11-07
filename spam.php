a155d29dd0a7bba96496672eeda8e93862661b6b
DreamerDotID <49750854+DreamerDotID@users.noreply.github.com>
2022-11-07 22:02:30 +0700

    Update spam.php

--- spam.php
+++ spam.php
@@ -18,6 +18,7 @@ function spamCall($api, $nomer, $jumlah, $delay)
     $url = "https://spamertelpon.herokuapp.com/v1?key=" . $api . "&target=" . $nomer;
     $loop = 0;
     while ($loop < $jumlah) {
+        $total = $jumlah - 1;
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
@@ -28,8 +29,8 @@ function spamCall($api, $nomer, $jumlah, $delay)
             echo "Minimal beli Api Key! \n";
         } else {
             echo "\n";
-            echo $nomer . " Successfully sended by " . $loop + 1 . " times\n";
-            echo "Please Increase Your Delay to Prevent Error!";
+            echo $nomer . " Berhasil mengirim " . $loop + 1 . " kali\n";
+            echo "Tersisa sebanyak " . $total . " lagi!";
             echo "\n";
         }
         sleep($delay);
@@ -43,7 +44,7 @@ echo "Masukan Jumlah : ";
 $jumlah = trim(fgets(STDIN));
 echo "Masukan Key : ";
 $key = trim(fgets(STDIN));
-echo "Masukkan Delay :";
+echo "Masukkan Delay : ";
 $delay = trim(fgets(STDIN));
 
 
